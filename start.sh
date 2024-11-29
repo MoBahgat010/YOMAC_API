d02018f2d79c0613e0c1107107b7996b8f8503c5
Mohamed Bahgat <mbahgat503@gmail.com>
2024-11-29 13:04:50 +0200

    add

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
-python manage.py migrate
+
+echo "Starting Gunicorn..."
 gunicorn api.wsgi:application --bind 0.0.0.0:$PORT
